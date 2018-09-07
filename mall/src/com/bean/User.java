f1f940c446f9d3098b1723647d1c3a893df306d6
乐凯 <@LAPTOP-899KISBH>
2018-09-07 15:41:58 +0800

    awdqwdq

---
+++ mall/src/com/bean/User.java
@@ -0,0 +1,53 @@
+package com.bean;
+
+public class User {
+	private int id;
+	private String name;
+	private String psw;
+	private int age;
+	private char sex;
+	private String adress;
+	private int phone;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPsw() {
+		return psw;
+	}
+	public void setPsw(String psw) {
+		this.psw = psw;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public char getSex() {
+		return sex;
+	}
+	public void setSex(char sex) {
+		this.sex = sex;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+}
